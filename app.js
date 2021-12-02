325efe812df85fb739c0d00e5703a115c6a96064
Gregory Villmann <gregory.villmann@khk.ee>
2021-12-02 09:47:59 +0200

    Displayed city weather data from API

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-// api.openweathermap.org/data/2.5/weather?q=tartu&appid=3f68bed6ed513095148620e7453abb2e
-
 
 function getWeather(){
     let APIEndpoint = 'https://api.openweathermap.org/data/2.5/weather?q=tartu&appid=3f68bed6ed513095148620e7453abb2e';
     fetch(APIEndpoint).then(function (response){
         return response.json();
     }).then(function(weatherData){
-        console.log(weatherData)
+        let temp = document.querySelector(".temp");
+        let location = document.querySelector(".location");
+        let desc = document.querySelector(".description");
+        desc.textContent = weatherData.weather[0].description;
+        location.textContent = weatherData.name;
+        temp.textContent = kelvinToCelsius(weatherData.main.temp);
     }).catch(function (err){
         console.log(err);
     })
 }
+
+function kelvinToCelsius(kelvin){
+    return Math.round(kelvin-273.1) + '\xB0';
+}
+getWeather();
